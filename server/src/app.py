b17da088fa865966d1889144fa5cea62aea221a1
Icaro <icaro.moreira@protonmail.com>
2021-03-29 17:51:43 -0300

    Implementação da autenticação das rotas

--- server/src/app.py
+++ server/src/app.py
@@ -1,23 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from passlib.hash import sha256_crypt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import verify_jwt_in_request
 import sqlite3
-import bcrypt
 import random
 import string
 import json
 
+
+
 APP = Flask(__name__)
 
+APP.config["JWT_SECRET_KEY"] = "qualquer coisa"
+
 CORS(APP)
+    
 cors = CORS(APP, resources = {
     r"/*": {
         "origins": "*"
     }
 })
 
+JWTManager(APP)
 
 @APP.route("/", methods = ["GET", "POST"])
 def home():
+
     if (request.method == "POST"):
         requestParsed = request.get_json()
         return jsonify({"situation": "OK"}, requestParsed)
@@ -25,7 +36,93 @@ def home():
     elif (request.method == "GET"):
         return jsonify({"message": "main page"})
 
-@APP.route("/senhas", methods = ["GET", "POST"])
+
+@APP.route("/cadastro", methods = ["POST"])
+def usuario():
+
+    requestParsed = request.get_json()
+
+    password = str(requestParsed["password"])
+    username = str(requestParsed["username"])
+    email = str(requestParsed["email"])
+
+    hashed_password = sha256_crypt.hash(password)
+
+
+
+    connection = sqlite3.connect("../database/passwordsDatabase.sqlite")
+    CUR = connection.cursor()
+
+    CUR.execute(""" 
+            CREATE TABLE IF NOT EXISTS "users" (
+                "user_ID"	    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "username"      TEXT NOT NULL UNIQUE,
+                "email"         TEXT NOT NULL UNIQUE,
+                "password"	    TEXT NOT NULL
+            );
+    """)
+
+    try:
+        CUR.execute("INSERT INTO users (username, email, password) VALUES (?,?,?)",
+                    [username, email, hashed_password])
+
+        connection.commit()
+        connection.close()
+
+        return json.dumps({"message": "sucesso", "conteudo": {"username": username, "email": email, "password": hashed_password}})
+
+    except sqlite3.IntegrityError:
+
+        return jsonify({"message": "erro, usuário já cadastrado"}), 400
+
+@APP.route("/login", methods = ["POST"])
+def login():
+    
+    requestParsed = request.get_json()
+    request_username = str(requestParsed["username"])
+    request_password = str(requestParsed["password"])
+
+    connection = sqlite3.connect("../database/passwordsDatabase.sqlite")
+    CUR = connection.cursor()
+
+    try:
+
+        CUR.execute("SELECT * FROM users WHERE username = ?", [request_username])
+        data = CUR.fetchone()
+        connection.commit()
+        connection.close()
+
+        id = data[0]
+        username = data[1]
+        email = data[2]
+        password = data[3]
+
+
+        if (sha256_crypt.verify(request_password, password)):
+
+            access_token = create_access_token(identity = id)
+
+            return jsonify({
+                "access_token": access_token,
+                "message": "correct password",
+            }), 200
+
+        else:
+            return jsonify({
+                "message": "wrong password or username"
+            }), 401
+    
+    except TypeError:
+        return jsonify({
+                "message": "wrong password or username"
+            }), 401
+
+
+
+
+@APP.route("/senhas", methods = ["GET", "POST", "DELETE"])
+@jwt_required()
+
 def password():
     if (request.method == "POST"):
         requestParsed = request.get_json()
@@ -41,7 +138,8 @@ def password():
         CUR.execute(""" 
                 CREATE TABLE IF NOT EXISTS "passwords" (
                     "password_ID"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    "password"	TEXT NOT NULL
+                    "user_ID"	    INTEGER NOT NULL,
+                    "password"	    TEXT NOT NULL
                 );
         """)
 
