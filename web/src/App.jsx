d18a55e912672c92acd6e4cabaf41913be4301fc
Ícaro <icaro.moreira@protonmail.com>
2021-10-02 22:44:54 -0300

    att

--- web/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Routes from "./Routes"
-
-export default function App(props) {
-
-    return (
-        <Routes></Routes>
-    )
-}
--- web/src/services/API.js
+++
@@ -1,7 +0,0 @@
-import Axios from "axios"
-
-const API = Axios.create({
-    baseURL: "http://localhost:5005"
-})
-
-export default API
