a495cd6af99ffed7049a52bdd6f2d7d1f9585377
L3G4S0V <l3g4s0v@pop-os.localdomain>
2022-04-17 22:08:15 -0300

    [Icaro] finishing the implementation of responsivity

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import {  Link  } from "react-router-dom"
-import { ArrowForwardIos } from '@material-ui/icons';
 import passwardenLogo from "../../assets/passwardenLogo3.png"
+
 import "./Header.css"
 
 export default function Header(props){
     return (
         <div id="header">
-
             <Link to="/" id="header-logo-wrapper">
                 <img id="header-logo" src={passwardenLogo} alt="" />
             </Link>
